1d081b2ca12d8b4a66f7017978c5f34138cb403c
MinhThu <mttnguyen91@gmail.com>
2022-08-18 13:10:28 -0500

    Color code past hours timeblock

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,42 @@
-// Display c 
-// var today = dayjs();
-// dayjs.extend(window.dayjs_plugin_advancedFormat);
-// dayjs.extend(window.dayjs_plugin_weekOfYear);
+// *Criteria* 
+// GIVEN I am using a daily planner to create a schedule
+// WHEN I open the planner
+// THEN the current day is displayed at the top of the calendar
+// WHEN I scroll down
+// THEN I am presented with time blocks for standard business hours
+// WHEN I view the time blocks for that day
+// THEN each time block is color-coded to indicate whether it is in the past, present, or future
+// WHEN I click into a time block
+// THEN I can enter an event
+// WHEN I click the save button for that time block
+// THEN the text for that event is saved in local storage
+// WHEN I refresh the page
+// THEN the saved events persist
 
-// $("#currentDay").text(today.format("MMM Do, YYYY"));
-
-// const today = document.querySelector("#currentDay");
-// console.log(today)
-// $("#currentDay") = dayjs(today.format("MMM Do, YYYY"));
-
-
-// $("#currentDay").text(moment().format("🗓" + 'dddd MMMM Do, YYYY'+ "🗓"));
-
-// Dayjs was used instead of moment, per instructors request 
-
+// Dayjs was used here per instructors request
 const today = dayjs();
-console.log(today.format("MMM D, YYYY"));
+// console.log(today.format("MMM D, YYYY"));
 $("#currentDay").text(today.format("🗓" + "dddd, MMMM D, YYYY" + "🗓"));
+$("#currentTime").text(today.format("h:mm A"));
 
+
+
+function timeColor(){
+    var hour = moment().hours();
+
+    $(".timeblocks").each(function(){
+        var currentHour = parseInt($(this).attr("id"));
+        console.log(this);
+    
+
+        if (currentHour > hour) {
+            $(this).addClass("future");
+        } else if (currentHour === hour) {
+            $(this).addClass("present");
+        } else {
+            $(this).addClass("past");
+        }
+    })
+};
+
+timeColor();
